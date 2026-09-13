e4f1aaaacc9c5bb5715e9dec2ed9a46b904df72f
Folombas <folombas@yandex.ru>
2026-09-13 22:34:14 +0300

    Исправили ошибки в коде

--- Program02.pas
+++ Program02.pas
@@ -7,8 +7,8 @@ begin
 	write('Введите число секунд: ');
 	read(t);
 	ch := t div (60*60);
-	t := mod (60*60);
+	t := t mod (60*60);
 	m := t div 60;
-	d := t mod 60;
+	s := t mod 60;
 	write(ch, ' ч.' ,m, ' мин.', s, ' c.');
 end.
